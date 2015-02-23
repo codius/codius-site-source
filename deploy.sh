d27c50860cdfb3cfdbc1c3176f9fa81ee35b873e
Torrie Fischer <torrie@ripple.com>
2015-02-23 11:17:32 -0800

    fix region

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ docker push quay.io/codius/codius.org:latest
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --region us-east-1 --application-name codius-dot-org --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
-aws elasticbeanstalk update-environment --region us-east-1 --environment-name codius-dot-org-env --version-label $SHA1
+aws elasticbeanstalk update-environment --region us-west-1 --environment-name codius-dot-org-env --version-label $SHA1
